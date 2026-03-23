f4dd739b575d8284b27c305c483ef207a09f899f
Julietkaaaaaaaa <162048724+Julietkaaaaaaaa@users.noreply.github.com>
2026-03-23 13:24:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+(function () {
+  var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
+
+  console.log("--- Перевірка на першу літеру 'J' ---");
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+
+  // --- ДОДАТКОВИЙ ФУНКЦІОНАЛ (Пункт 1.2.3) ---
+  console.log("\n--- Додатковий функціонал ---");
+  console.log("Якщо ім'я закінчується на літеру 'a', програма прощається, інакше - вітається.");
+  
+  for (var j = 0; j < names.length; j++) {
+    var currentName = names[j];
+    // Отримуємо останню літеру імені
+    var lastLetter = currentName.charAt(currentName.length - 1).toLowerCase();
+
+    if (lastLetter === 'a') {
+      byeSpeaker.speak(currentName);
+    } else {
+      helloSpeaker.speak(currentName);
+    }
+  }
+})();
